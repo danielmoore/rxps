e63b0ccf2aca3f0dd8e5365be45e571904dabc15
Daniel Moore <polaris@northhorizon.net>
2011-10-04 10:36:56 -0300

    Using PSv3 object creation

--- testscripts/Test-SelectObservableObject.ps1
+++ testscripts/Test-SelectObservableObject.ps1
@@ -1,3 +1,3 @@
 Write-TestExpectation "Single value with foo = 5"
-$foo = New-Object psobject -Property @{ foo = 5; bar = 6 }
+$foo = [PSCustomObject] @{ foo = 5; bar = 6 }
 New-Observable -Value $foo | Select-ObservableObject -Property foo | Start-Observable
